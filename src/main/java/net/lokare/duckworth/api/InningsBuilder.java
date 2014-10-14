3e844c29b3155dcd7397b6d48044f41841096521
Ameya Lokare <lokare.ameya@gmail.com>
2014-10-13 21:08:41 -0700

    Add javadoc

--- src/main/java/net/lokare/duckworth/api/InningsBuilder.java
+++ src/main/java/net/lokare/duckworth/api/InningsBuilder.java
@@ -11,6 +11,9 @@ import java.util.List;
 
 /**
  * @author ameya
+ *         <p/>
+ *         A builder object for {@link Innings}. Obtain an instance of this builder by calling {@link
+ *         InningsBuilder#allottedOvers}
  */
 public class InningsBuilder {
 
@@ -25,11 +28,28 @@ public class InningsBuilder {
         this.interruptions = new ArrayList<Interruption>();
     }
 
+    /**
+     * Obtain an instance of {@link InningsBuilder} with the specified number of initally allotted overs for the
+     * innings.
+     *
+     * @param overs the number of initially allotted overs for the innings
+     * @return an instance of {@link InningsBuilder}
+     */
     public static InningsBuilder forAllottedOvers(final int overs) {
         return new InningsBuilder(Overs.of(overs));
     }
 
-    public InningsBuilder interrupted(final Score score, final Overs completed, final Overs lost) {
+    /**
+     * Detail an interruption (rain, light malfunction etc) that halted the game. Provide the score when the
+     * interruption happened and the number of overs lost due to the interruption.
+     *
+     * @param score     the score when the match was interrupted
+     * @param completed the number of overs completed when the match was interrupted
+     * @param lost      the number of overs lost due to the interruption
+     * @return
+     */
+    public InningsBuilder interrupted(final Score
+            score, final Overs completed, final Overs lost) {
         final Interruption interruption = new Interruption(score, this.remaining.subtract(completed), lost,
                 ResourceTables.getStandardEdition());
         this.interruptions.add(interruption);
@@ -37,11 +57,22 @@ public class InningsBuilder {
         return this;
     }
 
+    /**
+     * Specify the score that the innings ended on.
+     *
+     * @param score the score that the innings ended on
+     * @return
+     */
     public InningsBuilder ended(final Score score) {
         this.finalScore = score;
         return this;
     }
 
+    /**
+     * Build the {@link Innings} object.
+     *
+     * @return the {@link Innings} object that was built
+     */
     public Innings build() {
         final Score score;
         if (this.finalScore != null) {
